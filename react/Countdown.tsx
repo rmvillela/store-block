5a802d49eabe7f5479aabd37b0b4d7e954421e3e
Roosevelt Villela <roosevelt@peopleinteractive.com.br>
2020-08-12 10:10:31 -0300

    implementação de countdown

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TimeSplit } from './typings/global'
+import { tick } from './utils/time'
 
 interface CountdownProps { 
   targetDate: string
 }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate }) => {
-  return (<div>
-    <h1>{ targetDate }</h1>
-  </div>)
+const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString()
+
+//react/Countdown.tsx
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  })
+
+  tick(targetDate, setTime)
+
+  return (
+    <div>
+      <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
+    </div>
+  ) 
 }
 
 Countdown.schema = {
